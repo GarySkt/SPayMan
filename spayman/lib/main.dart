717322d25d54087d68aa99c4bcf816089e5fc1c5
Gary Calle <gjcalle@upt.pe>
2020-04-27 20:27:44 -0500

    launcher actualizado

--- spayman/lib/main.dart
+++ spayman/lib/main.dart
@@ -116,4 +116,18 @@ class _MyHomePageState extends State<MyHomePage> {
       ),     
     );
   }
+
+  Widget showLogo() {
+    return new Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0.0, 70.0, 0.0, 0.0),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 48.0,
+          child: Image.asset('assets/icon/icon.png'),
+        ),
+      ),
+    );
+  }
 }
