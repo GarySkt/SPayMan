0f88eb7b46a6ce8a4b7c43273a06ec82ee249824
Gary Calle <gjcalle@upt.pe>
2020-04-29 16:48:18 -0500

    splashscreen add

--- spayman/lib/main.dart
+++ spayman/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:flutter/services.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
